f0b92eea4f03c60c221cc5dd07d3892b6ad783ed
alexeykosh <alexeykochevoy@gmail.com>
2016-11-08 17:31:35 +0300

    2

--- 1.py
+++ 1.py
@@ -1,7 +1,5 @@
 import urllib.request
 from bs4 import BeautifulSoup
-import lxml.html
-import re
 
 
 def create_request():
@@ -11,20 +9,22 @@ def create_request():
     case = input("грамм признаки: ") #типа через запятую %2
     case = case.replace(",", "%2C")
     url = 'http://search2.ruscorpora.ru/search.xml?env=alpha&mycorp=&mysent=&mysize=&mysentsize=&mydocsize=&spd=&text=lexgramm&mode=%s&sort=gr_tagging&lang=ru&nodia=1&parent1=0&level1=0&lex1=%s&gramm1=%s&sem1=&sem-mod1=sem&sem-mod1=sem2&flags1=&m1=&parent2=0&level2=0&min2=1&max2=1&lex2=&gramm2=&sem2=&sem-mod2=sem&sem-mod2=sem2&flags2=&m2=&out=%s'
-    url = url % (corpora, request, case, 'kwic')
-    print(url)
+    common_url = url % (corpora, request, case, 'kwic')
+    return common_url
 
 
 def get_table(url):
-    text = urllib.request.urlopen(url)
-    text = text.read().decode('windows-1251')
-    table_regex = '<span class=.*?>(.*?)<\/span>'
-    results = re. findall(table_regex, text, re.DOTALL)
-    print(results)
+    url = urllib.request.urlopen(url)
+    soup = BeautifulSoup(url, 'lxml')
+    res = soup.findAll("table")[1]
+    res = str(res)
+    Html_file = open("table.html", "a")
+    Html_file.write(res)
+    Html_file.close()
 
 
 def main():
-    create_request()
+    get_table(create_request())
 
 if __name__ == "__main__":
     main()
