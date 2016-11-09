62a440900f0f71390e32b3906d05bc44bde5ec11
alexeykosh <alexeykochevoy@gmail.com>
2016-11-09 13:09:05 +0300

    4

--- 1.py
+++ 1.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 
-def inputs():
+def inputs(): #здесь входные данные
     needs = []
     corpora = input("корпус: ")
     needs.append(corpora)
@@ -11,35 +11,38 @@ def inputs():
     request = urllib.request.quote(word.encode('windows-1251'))
     needs.append(request)
     case = input("грамм признаки: ")  # типа через запятую %2
-    case = case.replace(",", "%2C")
+    case = case.replace("", "%2C")
     needs.append(case)
     return needs
 
 
-def create_request(needs):
+def create_request(needs): #создаем ссылку поиска
     corpora = needs[0]
     request = needs[1]
     case = needs[2]
     url = 'http://search2.ruscorpora.ru/search.xml?env=alpha&mycorp=&mysent=&mysize=&mysentsize=&mydocsize=&spd=&text=lexgramm&mode=%s&sort=gr_tagging&lang=ru&nodia=1&parent1=0&level1=0&lex1=%s&gramm1=%s&sem1=&sem-mod1=sem&sem-mod1=sem2&flags1=&m1=&parent2=0&level2=0&min2=1&max2=1&lex2=&gramm2=&sem2=&sem-mod2=sem&sem-mod2=sem2&flags2=&m2=&out=%s'
     common_url = url % (corpora, request, case, 'kwic') #&p= что-то там
-    print(common_url)
+    print("ссылка:", common_url)
     return common_url
 
 
-def get_page_numbers(common_url):
+def get_page_numbers(common_url): #тут я получаю количество страниц
     common_url=common_url.replace('=kwic','')
     where_to_find = urllib.request.urlopen(common_url)
     text = where_to_find.read().decode('windows-1251')
-    q_regex = r'<span class="stat-number">(.*)<\/span>.*документов'
-    num_of_pages = re.findall(q_regex, text)[1]
+    q_regex = r'Найдено <span class="stat-number">(\d+[ ]?[\d+]?)'
+    num_of_pages = re.findall(q_regex, text)
+    num_of_pages = str(num_of_pages)
     num_of_pages = num_of_pages.replace(' ', '')
-    if num_of_pages == 0:
-        num_of_pages = 1
+    num_of_pages = num_of_pages.replace('\'', '')
+    num_of_pages = num_of_pages.replace('[', '')
+    num_of_pages = num_of_pages.replace(']', '')
+    num_of_pages = int(num_of_pages) // 10 + 1
     print(num_of_pages)
 
 
-def get_all_pages(common_url):
-    k = 1
+def get_all_pages(common_url): #тут у нас ссылки на все страницы
+    k = 0
     massive_of_links = []
     while k < 10:
         page = common_url + '&p=' + str(k)
@@ -48,7 +51,7 @@ def get_all_pages(common_url):
     return massive_of_links
 
 
-def get_table(urls):
+def get_table(urls): #тут вытаскиваем таблицу (сделал до 10 страниц чтобы не нагружать корпус)
     html_file = open("table.html", "w")
     for url in urls:
         soup_url = urllib.request.urlopen(url)
@@ -68,10 +71,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-    #page = requests.get(url)
-    #tree = html.fromstring(page.content)
-    #left_part = tree.xpath('/table/tr/td[1]|/table/tr/td[3]')
-    #print(left_part)
 
-
-#http://search2.ruscorpora.ru/search.xml?env=alpha&mycorp=&mysent=&mysize=&mysentsize=&mydocsize=&dpp=&spp=&spd=&text=lexform&mode=paper&sort=gr_tagging&lang=ru&req=%(здесь типа запрос)
