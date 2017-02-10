5c16a0693b41f5f3f121cb950924108e61e37b75
gaurwaithmelui <strategy155@gmail.com>
2017-02-10 18:55:33 +0300

    reorganized original function algs changed requests with selenium(chromedriver) remember about webdriver

--- lingcorpora/czh_corpus.py
+++ lingcorpora/czh_corpus.py
@@ -1,45 +1,54 @@
-from requests import get, post
+from selenium import webdriver
 
 
-def get_results():
-    parametres = {'shuffle': '1',
-                  'reload': '',
-                  'corpname': 'omezeni%2Fsyn2015',
-                  'queryselector': 'iqueryrow',
-                  'iquery': 'pozor',
-                  'lemma': '',
-                  'lpos': '',
-                  'phrase': '',
-                  'word': '',
-                  'wpos': '',
-                  'char': '',
-                  'cql': '',
-                  'default_attr': 'word',
-                  'fc_lemword_window_type': 'left',
-                  'fc_lemword_wsize': '1',
-                  'fc_lemword': '',
-                  'fc_lemword_type': 'all',
-                  'fc_pos_window_type': 'left',
-                  'fc_pos_wsize': '1',
-                  'fc_pos_type': 'all',
-                  'sca_doc.title': '',
-                  'sca_doc.author': '',
-                  'sca_doc.translator': ''}
-    user_agent ={'Host': 'kontext.korpus.cz',
-               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:51.0) Gecko/20100101 Firefox/51.0',
-               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-               'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-               'Accept-Encoding': 'gzip, deflate, br',
-               'Referer': 'https://kontext.korpus.cz/first_form',
-               'Cookie': 'kontextsid=039f6dd8ea91c436130493c03f4591444bd06e57',
-               'Connection': 'keep-alive',
-               'Upgrade-Insecure-Requests': '1'}
-    r = get(url='https://kontext.korpus.cz', data=parametres)
-    print(r.url)
+CORP_URL='https://kontext.korpus.cz'
+CLASS_NAMES = {'word':'query', 'corpus':'qselector'}
+
+
+def get_page_src(*args):
+    _driver = _driver_init()
+    _arg_list = _make_keys_lists(args)
+    _send_all_keys_to_forms(_driver, CLASS_NAMES.values(), _arg_list)
+    _wait_till_load(_driver)
+    html_src = _driver.page_source
+    _driver.close()
+    return html_src
+
+
+def _make_keys_lists(args):
+    output_list = []
+    for arg,class_name in zip(args,CLASS_NAMES.values()):
+        output_list.append(arg)
+    return output_list
+
+
+def _wait_till_load(queued_webdriver):
+    _query_string = queued_webdriver.current_url.split('/')[3]
+    while not _query_string.startswith('view'):
+        _query_string = queued_webdriver.current_url.split('/')[3]
+        continue
+    return None
+
+
+def _send_all_keys_to_forms(webdriver, form_html_classes, keys):
+    for key,form_html_class in zip(keys,form_html_classes):
+        print(key, form_html_class)
+        form = webdriver.find_element_by_class_name(form_html_class)
+
+        form.send_keys(key)
+    triggering_form = webdriver.find_element_by_class_name('query')
+    triggering_form.submit()
+    return None
+
+
+def _driver_init(driver_type='Chrome'):
+    driver = eval('webdriver.'+ driver_type)()
+    driver.get(CORP_URL)
+    return driver
 
 
 def main():
-    get_results()
+    get_page_src('tata','Basic')
 
 if __name__ == '__main__':
     main()
