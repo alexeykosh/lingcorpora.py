8b80348adefcf9191e3e7c91886d65f233322db2
alexeykosh <alexeykochevoy@gmail.com>
2017-03-14 15:36:13 +0300

    add comments

--- lingcorpora/pl_corpus.py
+++ lingcorpora/pl_corpus.py
@@ -9,7 +9,7 @@ def not_kwic(x):
     return x[0] + x[1] + x[2]
 
 
-def get_results(query, corpus, n_results, tag):
+def get_results(query, corpus, n_results, tag):  # the request part: here we are using request package
     if tag is True:
         tag_variable = 'slt'
     else:
@@ -21,7 +21,7 @@ def get_results(query, corpus, n_results, tag):
                        'wide_context_width': '50',
                        'results_per_page': n_results,
                        'next': '/poliqarp/' + corpus + '/query/'}
-    s = post(url='http://nkjp.pl/poliqarp/settings/', data=settings_params)
+    s = post(url='http://nkjp.pl/poliqarp/settings/', data=settings_params)  # post cookies
     user_agent = {'Host': 'nkjp.pl',
                   'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:51.0) Gecko/20100101 Firefox/51.0',
                   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -32,15 +32,15 @@ def get_results(query, corpus, n_results, tag):
                   'Connection': 'keep-alive',
                   'Upgrade-Insecure-Requests': '1'}
     post(url='http://nkjp.pl/poliqarp/query/', headers=user_agent, data={'query': query, 'corpus': 'nkjp300'})
-    r = get(url='http://nkjp.pl/poliqarp/nkjp300/query/', headers=user_agent)
+    r = get(url='http://nkjp.pl/poliqarp/nkjp300/query/', headers=user_agent)  # getting the final results
     return r
 
 
-def kwic_results(page, write, kwic):
+def kwic_results(page, write, kwic):  # results parse
     left_list = []
     right_list = []
     center_list = []
-    soup = BeautifulSoup(page.text, 'lxml')
+    soup = BeautifulSoup(page.text, 'lxml')  # getting the soup variable with the html-code from the results page
     for left in soup.select('.left'):
         left_list.append(left.text)
     for center in soup.select('span > a'):
