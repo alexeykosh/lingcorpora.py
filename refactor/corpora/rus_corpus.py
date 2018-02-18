a3af0facc7f2e535cea956a123b28ee233853460
akv17 <temakep@list.ru>
2018-02-18 21:02:35 +0300

    Update rus_corpus.py

--- refactor/corpora/rus_corpus.py
+++ refactor/corpora/rus_corpus.py
@@ -8,23 +8,6 @@ from time import sleep
 from collections import Iterable
 
 
-class Container:
-    def __init__(self,query,subcorpus=None,tag=False,nLeft=None,
-                 nRight=None,mode=None,numResults=100,kwic=True,
-                 session=None, language=None, targetLanguage=None, start=0):
-        self.language = language
-        self.targetLanguage = targetLanguage
-        self.query = query
-        self.subcorpus = subcorpus
-        self.tag = tag
-        self.nLeft = nLeft
-        self.nRight = nRight
-        self.mode = mode
-        self.numResults = numResults
-        self.kwic = kwic
-        self.start = start
-
-
 class Downloader(Container):
     def __init__(self, seed=None, *args,**kwargs):
         super().__init__(*args,**kwargs)
