164ee45f1d3f5c346301af98bebb05bb3b95e4ab
Maria Terekhina <maria.myslina@gmail.com>
2018-03-31 16:50:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='lingcorpora',
-      version='0.2',
-      description='API for National Corpus of Russian Language',
-      url='https://github.com/alexeykosh/lingcorpora.py',
-      author='Alexey Koshevoy',
+      version='1.0',
+      description='API for corpora',
+      url='https://github.com/lingcorpora/lingcorpora.py',
+      author='Alexey Koshevoy, Artem Kopetsky, Ekaterina Gerasemenko, Maria Terekhina',
       license='MIT',
       author_email='alexeykochevoy@gmail.com',
       packages=['lingcorpora'],
       zip_safe=False,
-      install_requires=['bs4', 'requests', 'pandas'])
+      install_requires=['bs4', 'requests', 'lxml', 'html', 'urllib', 're'])
