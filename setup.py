a66d58b37dbf0141e7c8c7447ff6c34e74f78a6e
Maria Terekhina <maria.myslina@gmail.com>
2018-04-01 23:03:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='lingcorpora',
-      version='1.0',
+      version='1.1',
       description='API for corpora',
       url='https://github.com/lingcorpora/lingcorpora.py',
       author='Alexey Koshevoy, Artem Kopetsky, Ekaterina Gerasimenko, Maria Terekhina',
       license='MIT',
       author_email='alexeykochevoy@gmail.com',
-      packages=['lingcorpora'],
+      packages=['lingcorpora', 'lingcorpora.corpora'],
       zip_safe=False,
       keywords = ['corpora', 'api', 'language'],
       install_requires=['bs4', 'requests', 'lxml', 'tqdm'])
