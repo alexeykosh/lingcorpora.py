89f5cf79000cece7a16af50a37ebb18d5d64ef09
akv17 <temakep@list.ru>
2018-06-23 22:38:08 +0300

    refactored_routine

--- test_lingcorpora/test_lc.py
+++ test_lingcorpora/test_lc.py
@@ -3,6 +3,8 @@ import os
 import unittest
 import pickle
 from modulefinder import ModuleFinder
+from random import randint
+from tqdm import tqdm
 
 sys.path.insert(0, os.path.abspath('..'))
 from lingcorpora.corpus import Corpus, functions
@@ -11,46 +13,82 @@ from lingcorpora.corpus import Corpus, functions
 __author__ = 'akv17'
 __doc__ = \
 """
-unittest based routine for testing `lingcorpora`
-i.e. language processing modules in `lingcorpora/corpora` 
+unittest based routine for testing modules in `lingcorpora/corpora` 
 """
 
 
 class TestLangFunc(unittest.TestCase):
 
     """
-    test given language processing module for:
+    test given module for:
+        - 0. valid fetch data
         - 1. processing single query
-        - 2. matching number or results to desired one
-        - 3. processing multiple query
-        - 4. docstring availability
+        - 2. matching number of results to the desired one
+        - 3. processing multi query
+        - 4. docstring
         - 5. exploiting local scope only
         - 6. exploiting legit dependencies only
     """
 
     def set_env(self, func, lang):
         """
-        pass language processing module to test 
-        as `func` <callable>, `lang` <str>
+        pass module to test as `func` <callable>, `lang` <str>
         """
         self.func = func
         self.lang = lang
-        self.corp = Corpus(self.lang)
-    
+        self.corp = Corpus(self.lang, verbose=False)
+        self.fetch_data = None
+
+    def test_fetch_data(self):
+        """
+        *** ALWAYS TO BE RUN PRIMARILY ***
+
+        assert that fetch data is valid
+        set as `self.fetch` if valid
+        """
+
+        _var = 'TEST_QUERIES'
+        self.assertIn(_var, self.func.__dict__)
+        self.assertIsInstance(self.func.__dict__.get(_var), dict)
+        self.assertIn('test_single_query', self.func.__dict__.get(_var).keys())
+        self.assertIsInstance(self.func.__dict__.get(_var).get('test_single_query'), str)
+        self.assertIn('test_multi_query', self.func.__dict__.get(_var).keys())
+        self.assertIsInstance(self.func.__dict__.get(_var).get('test_multi_query'), list)
+        self.assertIsInstance(self.func.__dict__.get(_var).get('test_single_query')[0], str)
+
+        TestLangFunc.fetch_data = self.func.__dict__.get(_var)
+
     def test_single_query(self):
-        case = 'test_single_query'
-        self.assertIn(case, self.func.__dict__)
-        self.func.test_single_query()
+        query = self.fetch_data.get('test_single_query')
+
+        parser = self.func.PageParser(query=query,
+                                      numResults=3
+                                      )
+
+        for t in parser.extract():
+            l, r = t.idxs
+            assert t.text[l:r].lower() == query.lower(), \
+                'does not match query: `%s`' % t.text[l:r]
+
+        del parser
 
     def test_num_results(self):
-        case =  'test_num_results'
-        self.assertIn(case, self.func.__dict__)
-        self.func.test_num_results()
+        query = self.fetch_data.get('test_single_query')
+        numResults = randint(1, 5)
+
+        parser = self.func.PageParser(query=query,
+                                      numResults=numResults
+                                      )
+
+        res = list(parser.extract())
+
+        assert len(res) == numResults
+
+        del parser, res
 
     def test_multi_query(self):
-        case =  'test_multi_query'
-        self.assertIn(case, self.func.__dict__)
-        query = self.func.test_multi_query()
+        query = self.fetch_data.get('test_multi_query')
+
         R = self.corp.search(query=query,
                              numResults=1
                              )
@@ -63,17 +101,18 @@ class TestLangFunc(unittest.TestCase):
         del R
         
     def test_docstring(self):
-        case = '__doc__'
-        self.assertIn(case, self.func.__dict__)
+        self.assertIn('__doc__', self.func.__dict__)
         self.assertIsInstance(self.func.__doc__, str)
 
     def test_local_scope_only(self):
-        case = 'test_local_scope_only'
-        query = self.func.test_multi_query()[0]
+        query = self.fetch_data.get('test_single_query')
+
         pre_globals_len = len(globals())
+
         R = self.corp.search(query=query,
                              numResults=1
                              )
+
         post_globals_len = len(globals())
 
         self.assertEqual(pre_globals_len,
@@ -83,13 +122,28 @@ class TestLangFunc(unittest.TestCase):
         del R
 
     def test_dependencies(self):
-        path = os.path.join('lingcorpora',
+        """
+        EXTREMELY SLOW
+        """
+
+        path = os.path.join('..',
+                            'lingcorpora',
                             'corpora',
                             '%s_corpus.py' % self.lang
                             )
-        legit_deps = pickle.load(open('dependencies.pickle', 'rb'))
-        
+
+        core_path = os.path.join('..',
+                                 'lingcorpora',
+                                 'corpus.py',
+                                 )
+
+        # legit_deps = pickle.load(open('dependencies.pickle', 'rb'))
+
         mf = ModuleFinder()
+
+        mf.run_script(init_path)
+        legit_deps = set(mf.modules.keys())
+
         mf.run_script(path)
         deps = set(mf.modules.keys())
 
@@ -109,7 +163,7 @@ def run(funcs_to_test=None, tests_to_run=None, stream=None, verbosity=2):
                                               if None `TESTS_TO_RUN` passed
         - stream: file-like: stream to verbose to
                                   if None `sys.stderr` passed
-        - vebosity: int: verbosity mode
+        - verbosity: int: verbosity mode
     """
     
     if funcs_to_test is None:
@@ -131,24 +185,27 @@ def run(funcs_to_test=None, tests_to_run=None, stream=None, verbosity=2):
         suite = unittest.TestSuite()
         routine = TestLangFunc
 
-        for _test in tests_to_run:
-            suite.addTest(routine(_test))
-
         routine.set_env(routine, FUNC, LANG)
 
-        runner.run(suite)
+        runner.run(routine('test_fetch_data'))
+
+        if routine.fetch_data is not None:
+            for _test in tests_to_run:
+                suite.addTest(routine(_test))
+
+            runner.run(suite)
             
     stream.close()
 
 if __name__ == '__main__':
-    #FUNCS_TO_TEST = functions
+    # FUNCS_TO_TEST = functions
     FUNCS_TO_TEST = {'rus': functions['rus']}
     TESTS_TO_RUN = ['test_single_query',
                     'test_num_results',
                     'test_multi_query',
                     'test_docstring',
                     'test_local_scope_only',
-                    'test_dependencies'
+                    # 'test_dependencies'
                     ]
 
     run()
