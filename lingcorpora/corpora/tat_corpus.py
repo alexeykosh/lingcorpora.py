68eec96cc121edd902462dde988d652ac9a147af
akv17 <temakep@list.ru>
2018-07-10 13:36:55 +0300

    Update tat_corpus.py

--- lingcorpora/corpora/tat_corpus.py
+++ lingcorpora/corpora/tat_corpus.py
@@ -10,10 +10,9 @@ results_url = 'http://web-corpora.net/TatarCorpus/search/results.php'
 
 # get_meta
 
-
-TEST_QUERIES = {'test_single_query': 'туган',
-                'test_multi_query': ['туган', 'мәхәббәт']
-                }
+TEST_DATA = {'test_single_query': {'query': 'туган'},
+             'test_multi_query': {'query': ['туган', 'мәхәббәт']}
+            }
 
 __doc__ = \
     """
