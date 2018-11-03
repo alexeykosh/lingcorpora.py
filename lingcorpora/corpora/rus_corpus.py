e33f95f5838ea8fd1a788d8b248ca44d979c01ac
akv17 <temakep@list.ru>
2018-11-03 18:28:32 +0300

    gram_search

--- lingcorpora/corpora/rus_corpus.py
+++ lingcorpora/corpora/rus_corpus.py
@@ -13,30 +13,130 @@ from ..exceptions import EmptyPageException
 
 
 __author__ = 'akv_17'
+
 __doc__ = \
 """
 API for National Corpus of Russian (http://ruscorpora.ru/index.html)
 
 Args:
     query: str or List([str]): query or queries (currently only exact search by word or phrase is available)
-    numResults: int: number of results wanted (100 by default)
+    num_results: int: number of results wanted (100 by default)
     kwic: boolean: kwic format (True) or a sentence (False) (True by default)
-    tag: boolean: whether to collect grammatical tags for target word or not (False by default)
+    analysis: boolean: whether to collect grammatical tags for target word or not (False by default)
     subcorpus: str: subcorpus ('main' by default).
                     Valid: ['main', 'syntax', 'paper', 'regional', 'school',
                             'dialect', 'poetic', 'spoken', 'accent', 'murco',
                             'multiparc', 'old_rus', 'birchbark', 'mid_rus', 'orthlib']
-    
+
 Main method: extract
 
 Returns:
     A generator over Target objects.
 """
 
+GR_TAGS_INFO = \
+"""
+Часть речи
+    существительное: S
+    прилагательное: A
+    числительное: NUM
+    числприл: ANUM
+    глагол: V
+    наречие: ADV
+    предикатив: PRAEDIC
+    вводное слово: PARENTH
+    местсущ: SPRO
+    местприл: APRO
+    местпредикатив: PRAEDICPRO
+    местоименное наречие: ADVPRO
+    предлог: PR
+    союз: CONJ
+    частица: PART
+    междометие: INTJ
+
+Падеж
+    именительный: nom
+    звательный: voc
+    родительный: gen
+    родительный 2: gen2
+    дательный: dat
+    винительный: acc
+    винительный 2: acc2
+    творительный: ins
+    предложный: loc
+    предложный 2: loc2
+    счётная форма: adnum
+
+Наклонение / Форма
+    изъявительное: indic
+    повелительное: imper
+    повелительное 2: imper2
+    инфинитив: inf
+    причастие: partcp
+    деепричастие: ger
+
+Степень / Краткость
+    сравнительная: comp
+    сравнительная 2: comp2
+    превосходная: supr
+    полная форма: plen
+    краткая форма: brev
+
+Время
+    настоящее: praes
+    будущее: fut
+    прошедшее: praet
+
+Переходность
+    переходный: tran
+    непереходный: intr
+
+Число
+    единственное: sg
+    множественное: pl
+
+Лицо
+    первое: 1p
+    второе: 2p
+    третье: 3p
+
+Прочее
+    цифровая запись: ciph
+    аномальная форма: anom
+    искаженная форма: distort
+    инициал: INIT
+    сокращение: abbr
+    несклоняемое: 0
+    топоним: topon
+
+Имена собственные
+    фамилия: famn
+    имя: persn
+    отчество: patrn
+
+Род
+    мужской: m
+    женский: f
+    средний: n
+    общий: mf
+
+Залог
+    действительный: act
+    страдательный: pass
+    медиальный: med
+
+Одушевленность
+    одушевленное: anim
+    неодушевленное: inan
+
+Вид
+    совершенный: pf
+    несовершенный: ipf
+"""
 
 TEST_DATA = {'test_single_query': {'query': 'фонема'},
              'test_multi_query': {'query': ['фонема', 'морфема']}
-            }
+}
 
 
 class PageParser(Container):
@@ -55,6 +155,7 @@ class PageParser(Container):
             
         self.__url = 'http://search1.ruscorpora.ru/dump.xml?'
         self.__request = 'env=alpha&nodia=1&mode=%s&text=lexform&sort=gr_tagging&seed=%s&dpp=%s&req=%s&p=%s'
+        self.__request_gr = 'env=alpha&nodia=1&mode=%s&text=lexgramm&sort=gr_tagging&seed=%s&dpp=%s&lex1=%s&gramm1=%s&p=%s'
 
     def __get_ana(self, word):
         """
@@ -117,14 +218,26 @@ class PageParser(Container):
         """
         return: etree of the page
         """
-        params = (self.subcorpus,
-                  self.__seed,
-                  self.__dpp,
-                  quote(self.query),
-                  page_num,
-                 )
 
-        request = self.__request % (params)
+        if self.gr_tags is not None:
+            arguments = (self.subcorpus,
+                         self.__seed,
+                         self.__dpp,
+                         quote(self.query),
+                         quote(self.gr_tags),
+                         page_num
+            )
+
+        else:
+            arguments = (self.subcorpus,
+                         self.__seed,
+                         self.__dpp,
+                         quote(self.query),
+                         page_num
+            )
+
+        request = self.__request_gr if self.gr_tags is not None else self.__request
+        request = request % (arguments)
         
         return parse(self.__url + request)
 
@@ -134,10 +247,10 @@ class PageParser(Container):
         if not docs_tree:
             raise EmptyPageException
     
-        for doc in self.__parse_docs(docs_tree, self.tag):
+        for doc in self.__parse_docs(docs_tree, self.analysis):
             self.__targets_seen += 1
             
-            if self.__targets_seen <= self.numResults:
+            if self.__targets_seen <= self.n_results:
                 yield Target(*doc) 
             
             else:
@@ -148,6 +261,7 @@ class PageParser(Container):
         """
         A streamer to Corpus
         """
+
         while not self.__stop_flag:
             try:
                 page = self.__get_page(self.__page_num)
